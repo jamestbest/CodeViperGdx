99532127c6b810797edfde64ec094642c1629d6e
jamestbest <james2004.coward@gmail.com>
2022-06-23 14:08:51 +0100

    added todo

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
+// TODO: 23/06/2022 i dont think it is erasing the old document that it creates so if you add another input it places both in the doc
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
